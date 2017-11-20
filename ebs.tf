216471a3f6c3ba36fd678c047007653f020657cc
Lance Kuo <lancekuo@gmail.com>
2017-11-20 18:52:18 -0500

    Do not mkfs and parted while it exist already. Make script compatible with dash shell.

--- ebs.tf
+++ ebs.tf
@@ -30,8 +30,8 @@ resource "null_resource" "ebs_trigger" {
     provisioner "remote-exec" {
         inline = [
             "if [ -d /opt/jenkins/ ];then echo \"The folder exists.\";else sudo mkdir /opt/jenkins;sudo chown -R ubuntu:ubuntu /opt/jenkins;sudo chmod 777 /opt/jenkins;echo \"Mount point created.\";fi",
-#            "sudo parted /dev/xvdg --script -- mklabel msdos mkpart primary ext4 0 -1",
-#            "sudo mkfs.ext4 -F /dev/xvdg1",
+            "if [ ! -b /dev/xvdg1 ]; then sudo parted /dev/xvdg --script -- mklabel msdos mkpart primary ext4 0 -1;fi",
+            "if [ ! \"$(sudo lsblk --fs /dev/xvdg1 --nodeps -o FSTYPE -t -n)\" = \"ext4\" ]]; then sudo mkfs.ext4 -F /dev/xvdg1;fi",
             "if ! grep -e \"$$(sudo file -s /dev/xvdg1|awk -F\\  '{print $8}')    /opt/jenkins\" /etc/fstab 1> /dev/null;then echo \"`sudo file -s /dev/xvdg1|awk -F\\  '{print $8}'`    /opt/jenkins    ext4    defaults,errors=remount-ro    0    0\"| sudo tee -a /etc/fstab;else echo 'Fstab has the mount point'; fi ",
             "if grep -qs '/opt/jenkins' /proc/mounts; then echo \"/opt/jenkins has mounted.\"; else sudo mount `sudo file -s /dev/xvdg1|awk -F\\  '{print $8}'` /opt/jenkins; fi",
         ]
